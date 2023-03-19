4286f966b9f21af14bf2be9fd29c4194efcfb740
Polasi Deva Raju <94927632+devarajupolasi@users.noreply.github.com>
2023-03-19 17:07:29 +0530

    dddd

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+Welocome to my web        </a>
       </header>
     </div>
   );
